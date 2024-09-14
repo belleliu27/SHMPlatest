de33b517d77ddeab5458b87cc13db56e8a7bed27
Belle <belleliu27@gmail.com>
2024-09-14 16:31:14 +1000

    latest

---
+++ client/src/App.js
@@ -0,0 +1,50 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ProtectedPage from "./components/ProtectedPage";
+import Spinner from "./components/Spinner";
+import { useSelector } from "react-redux";
+import Admin from "./pages/Admin";
+
+function App() {
+  const { loading } = useSelector((state) => state.loaders);
+  return (
+    <div>
+      {loading && <Spinner />}
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedPage>
+                <Home />
+              </ProtectedPage>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedPage>
+                <Profile />
+              </ProtectedPage>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedPage>
+                <Admin />
+              </ProtectedPage>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
